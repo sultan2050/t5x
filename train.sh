79cc67e4d330f9437c4b9fa7dd4383c896bc5d50
sultan2050 <big35manf@gmail.com>
2024-12-03 21:23:46 +0300

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-MODEL_DIR="gs://sultan-t5x/CaseReport_MIMIC_M3Vocab_base"
+MODEL_DIR="gs://sultan-t5x/kenlm_30GB_MIMIC_M3Vocab_base"
 python3 t5x/train.py   --gin_search_paths=clinc_t5   --gin_file="pretrain.gin"   --gin.MODEL_DIR=\"${MODEL_DIR}\"
